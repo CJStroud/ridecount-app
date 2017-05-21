8f3d93b03b05d92ad00f9f2ec4c745b7ffe8c26b
Chris Stroud <chris.stroud@drpgroup.com>
2017-05-21 18:02:08 +0100

    :heavy_plus_sign: Add react native navigation with example

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { Navigation } from 'react-native-navigation';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { registerScreens } from './screens';
 
-export default class RidecountApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+registerScreens(); // this is where you register all of your app's screens
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+// start the app
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One',
+      screen: 'example.FirstTabScreen', // this is a registered name for a screen
+      // icon: require('../img/one.png'),
+      // selectedIcon: require('../img/one_selected.png'), // iOS only
+      title: 'Screen One'
+    },
+    {
+      label: 'Two',
+      screen: 'example.SecondTabScreen',
+      // icon: require('../img/two.png'),
+      // selectedIcon: require('../img/two_selected.png'), // iOS only
+      title: 'Screen Two'
+    }
+  ]
 });
-
-AppRegistry.registerComponent('RidecountApp', () => RidecountApp);
